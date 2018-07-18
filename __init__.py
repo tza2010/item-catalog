c5b87aedec41f45bdc09964784b2e1f5705f8718
Mohammed Moataz <tzawy2010@gmail.com>
2018-07-18 16:00:31 +0000

    fixed google+ permissions

--- __init__.py
+++ __init__.py
@@ -154,7 +154,7 @@ def gconnect():
 
     try:
         # Upgrade the authorization code into a credentials object
-        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow = flow_from_clientsecrets('/var/www/ItemCatalog/ItemCatalog/client_secrets.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
